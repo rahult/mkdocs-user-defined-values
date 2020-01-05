d3468e8cb82a07dbaa60d8870c077f4d8f65a2c0
Rahul Trikha <rahul.trikha@rea-group.com>
2020-01-05 20:25:31 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# mkdocs-user-defined-values
+
+This is a plugin for a MkDocs which allows pages to have user defined input values.
+
+## Setup
+
+Install the plugin using pip:
+
+`pip install mkdocs-user-defined-values`
+
+Activate the plugin in `mkdocs.yml`:
+```yaml
+plugins:
+  - search
+  - user-defined-values:
+      keywords:
+        - DOC_AS_CODE_PATH
+        - DOC_AS_CODE_VERSION
+```
+
+> **Note:** If you have no `plugins` entry in your config file yet, you'll likely also want to add the `search` plugin. MkDocs enables it by default if there is no `plugins` entry set, but now you have to enable it explicitly.
+
+More information about plugins in the [MkDocs documentation][mkdocs-plugins].
+
+## Config
+
+* `keywords` - This is a list of keywords which you want to allow a user to modify.
+* `input-placeholder` - This is a placeholder where you want the plugin to generate the form for user to provide dynamic values for each keyword. Default value is `{{{user-defined_values}}}`.
+
+## Usage
+
+- Provide a list of `keywords` you want a user to provide dynamically.
+- Place `{{{user-defined_values}}}` in your page or template where you want to generate the form for user to provide dynamic values.
+
+## See Also
+
+More information about templates [here][mkdocs-template].
+
+More information about blocks [here][mkdocs-block].
+
+[mkdocs-plugins]: http://www.mkdocs.org/user-guide/plugins/
+[mkdocs-template]: https://www.mkdocs.org/user-guide/custom-themes/#template-variables
+[mkdocs-block]: https://www.mkdocs.org/user-guide/styling-your-docs/#overriding-template-blocks
---
+++ plugin/plugin.py
@@ -0,0 +1,65 @@
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options
+from mkdocs.plugins import BasePlugin
+
+class UserDefinedValues(BasePlugin):
+
+    config_scheme = (
+        ('keywords', config_options.Type(list)),
+        ('input-placeholder', config_options.Type(mkdocs_utils.string_types, default='{{{user-defined_values}}}'))
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_post_page(self, output_content, page, config):
+        data_tag = 'data-bind-user-defined-values'
+        input_boxes = ''
+
+        # Wrap keyword with span and data tag
+        for keyword in self.config['keywords']:
+            output_content = output_content.replace(keyword, f'<span {data_tag}="{keyword}">{keyword}</span>')
+
+        # Embed binding javascript
+        input_boxes = '''
+        <style>
+            label.user-defined-values {
+                width: 30%
+            }
+
+            input.user-defined-values[type=text] {
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                box-sizing: border-box;
+                border: 1px solid black;
+                display: inline-block;
+            }
+        </style>
+        '''
+
+        for keyword in self.config['keywords']:
+            javascript_variable_name = keyword.lower()
+            input_boxes += f'''
+                <label class="user-defined-values" for="{keyword}">{keyword}</label>
+                <input class="user-defined-values" type="text" id="{keyword}" />
+                <script>
+                    const {javascript_variable_name} = document.getElementById('{keyword}');
+                    {javascript_variable_name}.oninput = function(e) {{
+                        const value = e.target.value;
+                        document.querySelectorAll('[{data_tag}="{keyword}"]').forEach(function(element, _) {{
+                            if (value == '') {{
+                                element.innerHTML = '{keyword}';
+                            }} else {{
+                                element.innerHTML = value;
+                            }}
+                        }});
+                    }};
+                </script>
+            '''
+
+        output_content = output_content.replace(self.config['input-placeholder'], input_boxes)
+
+        print(output_content)
+        return output_content
