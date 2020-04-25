b836869674ce92eeb865b6ca13bbfb3126de1d60
Rahul Trikha <rahul.trikha@rea-group.com>
2020-04-26 03:13:43 +1000

    Unit testing plugin defaults

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,24 +2,35 @@ import pytest
 from plugin.plugin import *
 
 
-@pytest.mark.parametrize(
-    "key,expected",
-    [("keywords", None), ("input-placeholder", "{{{user-defined-values}}}")],
-)
-def test_plugin_default_config(key, expected):
+def test_plugin_default_config():
+    expected = {"keywords": None, "input-placeholder": "{{{user-defined-values}}}"}
     plugin = UserDefinedValues()
     errors, warnings = plugin.load_config({})
-    assert plugin.config[key] == expected
+    assert plugin.config == expected
     assert errors == []
     assert warnings == []
 
 
 def test_plugin_config_input_placeholder():
-    expected = {"keywords": None, "input-placeholder": "{{{custom-placehoder}}}"}
+    expected = {"keywords": None, "input-placeholder": "{{{custom-placeholder}}}"}
     plugin = UserDefinedValues()
     errors, warnings = plugin.load_config(
         {"input-placeholder": "{{{custom-placeholder}}}"}
     )
-    assert sorted(plugin.config) == sorted(expected)
+    assert plugin.config == expected
+    assert errors == []
+    assert warnings == []
+
+
+def test_plugin_config_keywords():
+    expected = {
+        "keywords": {"YOUR_AWS_REGION": {"placeholder": "e.g. ap-southeast-2"}},
+        "input-placeholder": "{{{user-defined-values}}}",
+    }
+    plugin = UserDefinedValues()
+    errors, warnings = plugin.load_config(
+        {"keywords": {"YOUR_AWS_REGION": {"placeholder": "e.g. ap-southeast-2"}}}
+    )
+    assert plugin.config == expected
     assert errors == []
     assert warnings == []
