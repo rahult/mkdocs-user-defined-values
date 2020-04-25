ea99c76212b2a0bbf4b745bc12016daf002f2044
Rahul Trikha <rahul.trikha@rea-group.com>
2020-04-26 01:22:06 +1000

    Adding basic unit testing for plugin

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,25 @@
-def fun(x):
-    return x + 1
+import pytest
+from plugin.plugin import *
 
 
-def test_answer():
-    assert fun(3) == 4
+@pytest.mark.parametrize(
+    "key,expected",
+    [("keywords", None), ("input-placeholder", "{{{user-defined-values}}}")],
+)
+def test_plugin_default_config(key, expected):
+    plugin = UserDefinedValues()
+    errors, warnings = plugin.load_config({})
+    assert plugin.config[key] == expected
+    assert errors == []
+    assert warnings == []
+
+
+def test_plugin_config_input_placeholder():
+    expected = {"keywords": None, "input-placeholder": "{{{custom-placehoder}}}"}
+    plugin = UserDefinedValues()
+    errors, warnings = plugin.load_config(
+        {"input-placeholder": "{{{custom-placeholder}}}"}
+    )
+    assert sorted(plugin.config) == sorted(expected)
+    assert errors == []
+    assert warnings == []
